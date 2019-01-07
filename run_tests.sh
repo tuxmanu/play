739954fc88df21c4fa228b03fe10ea53e4ce9d94
Manuel Mink <manuel.mink@de.bosch.com>
2019-01-07 16:12:05 +0100

    ps outcommented

--- run_tests.sh
+++ run_tests.sh
@@ -2,8 +2,8 @@
 
 returncode=1
 
-ps ax | grep apache
-ps ax | grep httpd
+#ps ax | grep apache
+#ps ax | grep httpd
 
 #if telnet localhost 80; then returncode=0; fi
 if wget --tries=2 http://localhost/index.html; then returncode=0; fi
