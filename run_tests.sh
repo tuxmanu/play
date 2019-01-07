b740881ddc85bfd56a92457c3681ec85f0a81cf9
Manuel Mink <manuel.mink@de.bosch.com>
2019-01-07 15:49:37 +0100

    only tries a few times

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,6 @@ ps ax | grep apache
 ps ax | grep httpd
 
 #if telnet localhost 80; then returncode=0; fi
-if wget http://localhost/index.html; then returncode=0; fi
+if wget --tries=2 http://localhost/index.html; then returncode=0; fi
 
 echo $returncode
